0024fc83002287b723b03c698f5b3b3936477927
mwahyd <72066821+mwahyd@users.noreply.github.com>
2023-12-19 19:50:47 +0100

    solution to new problem

--- codewars.js
+++ codewars.js
@@ -255,3 +255,12 @@ function max(list) {
   }
   return currentMax;
 }
+
+// * Write a function which calculates the average of the numbers in a given list.
+
+function findAverage(array) {
+  if (array.length < 1) return 0;
+  return (
+    array.reduce((total, currentVal) => total + currentVal, 0) / array.length
+  );
+}
